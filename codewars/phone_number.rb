eea422e6563d4e3d04ec13bb14f7101c40cfe344
weremite <weremite@gmail.com>
2021-06-24 23:58:46 +0300

    fix some issues in "phone_number.rb"

--- codewars/phone_number.rb
+++ codewars/phone_number.rb
@@ -1,18 +1,17 @@
-=begin
-Write a function that accepts an array of 10 integers (between 0 and 9), 
-that returns a string of those numbers in the form of a phone number.
-Example:
+# frozen_string_literal: true
 
-createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+# Write a function that accepts an array of 10 integers (between 0 and 9),
+# that returns a string of those numbers in the form of a phone number.
+# Example:
+# createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
 
-=end
 def create_phone_number(numbers)
-  first_three_nums = numbers.take(3).join()
-  second_three_nums = numbers[3...6].join()
-  last_nums = numbers[6...].join()
+  first_three_nums = numbers.take(3).join
+  second_three_nums = numbers[3...6].join
+  last_nums = numbers[6...10].join
 
-  return "(#{first_three_nums}) #{second_three_nums}-#{last_nums}"
+  "(#{first_three_nums}) #{second_three_nums}-#{last_nums}"
 end
 
-puts create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) #"(123) 456-7890"
-puts create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) #"(111) 111-1111"
+puts create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # "(123) 456-7890"
+puts create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) # "(111) 111-1111"
